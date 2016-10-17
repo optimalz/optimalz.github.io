0841018133fb27225b63efa97894bd80d2422780
Peugui <paeugui@gmail.com>
2016-10-17 15:27:06 +0200

    carrousel

--- js/carouselScript2.js
+++ js/carouselScript2.js
@@ -23,6 +23,24 @@ function init(){
 	slider(ul);
 }
 
+ $(function(){
+    $.each(document.images, function(){
+               var this_image = this;
+               var src = $(this_image).attr('src') || '' ;
+               if(!src.length > 0){
+                   //this_image.src = options.loading; // show loading
+                   var lsrc = $(this_image).attr('lsrc') || '' ;
+                   if(lsrc.length > 0){
+                       var img = new Image();
+                       img.src = lsrc;
+                       $(img).load(function() {
+                           this_image.src = this.src;
+                       });
+                   }
+               }
+           });
+  });
+
 function slider(){		
 		animate({
 			delay:17,
