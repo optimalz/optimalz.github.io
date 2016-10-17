a9bd8510626c29682b7baed23d30ea82e36d7576
Peugui <paeugui@gmail.com>
2016-10-17 16:18:33 +0200

    carrousel

--- js/carouselScript2.js
+++ js/carouselScript2.js
@@ -23,24 +23,6 @@ function init(){
 	slider(ul);
 }
 
- $(function(){
-    $.each(document.images, function(){
-               var this_image = this;
-               var src = $(this_image).attr('src') || '' ;
-               if(!src.length > 0){
-                   //this_image.src = options.loading; // show loading
-                   var lsrc = $(this_image).attr('lsrc') || '' ;
-                   if(lsrc.length > 0){
-                       var img = new Image();
-                       img.src = lsrc;
-                       $(img).load(function() {
-                           this_image.src = this.src;
-                       });
-                   }
-               }
-           });
-  });
-
 function slider(){		
 		animate({
 			delay:17,
@@ -87,4 +69,24 @@ function animate(opts){
 		}
 	}, opts.dalay || 17);
 }
+
+
+ $(function(){
+    $.each(document.images, function(){
+               var this_image = this;
+               var src = $(this_image).attr('src') || '' ;
+               if(!src.length > 0){
+                   //this_image.src = options.loading; // show loading
+                   var lsrc = $(this_image).attr('lsrc') || '' ;
+                   if(lsrc.length > 0){
+                       var img = new Image();
+                       img.src = lsrc;
+                       $(img).load(function() {
+                           this_image.src = this.src;
+                       });
+                   }
+               }
+           });
+  });
+
 window.onload = init;
